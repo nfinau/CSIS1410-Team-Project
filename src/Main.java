ffb31ba6dcf88cc307c7275bc31e3de889b13eec
nfinau <omgitznaomi@gmail.com>
2026-04-15 15:52:28 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        TaskManager taskManager = new TaskManager();
+        GuiTaskTracker guiTaskTracker = new GuiTaskTracker(taskManager);
 
+        guiTaskTracker.showMenu();
+    }
+}
